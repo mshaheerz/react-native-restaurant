c190c752002e1dfc949a39709b4041474d4a19d9
mshaheerz <mshaheerkp9@gmail.com>
2023-11-22 20:12:27 +0530

    added delivery screeen and maps and some animations

--- navigation.js
+++ navigation.js
@@ -1,5 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator,TransitionPresets } from "@react-navigation/stack";
+import { createStackNavigator, TransitionPresets } from "@react-navigation/stack";
 const Stack = createStackNavigator();
 
 
@@ -9,25 +9,30 @@ import HomeScreen from "./screens/HomeScreen";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import CartScreens from "./screens/CartScreens";
 import { StatusBar } from "expo-status-bar";
+import OrderPreparingScreen from "./screens/OrderPreparingScreen";
+import DeliveryScreen from "./screens/DeliveryScreen";
 
 export default function Navigation() {
 
     return (
         <NavigationContainer>
             <StatusBar />
-            <Stack.Navigator   screenOptions={{
+            <Stack.Navigator screenOptions={{
                 headerShown: false,
-                
+            
+
             }}
 
             >
                 <Stack.Screen name="Home" component={HomeScreen} />
                 <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-                <Stack.Group screenOptions={{presentation:"modal"}}>
-                    <Stack.Screen  name="Cart"   options={{title:"cart",...TransitionPresets.ModalPresentationIOS,presentation:'modal'}} component={CartScreens} />
-                </Stack.Group>
-                
-               
+
+                <Stack.Screen name="Cart" options={{ title: "cart", ...TransitionPresets.ModalPresentationIOS, presentation: 'modal' }} component={CartScreens} />
+ 
+                <Stack.Screen name="OrderPreparing" options={{...TransitionPresets.ModalSlideFromBottomIOS}}  component={OrderPreparingScreen} />
+
+                <Stack.Screen name="DeliveryScreen" options={{...TransitionPresets.ModalSlideFromBottomIOS}}  component={DeliveryScreen} />
+
             </Stack.Navigator>
         </NavigationContainer>
     )
